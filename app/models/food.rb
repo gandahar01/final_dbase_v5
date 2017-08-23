056eecc6bf7eb2e2735e5c02f52f9169dfd7e7ea
Raghu Betina <raghu@firstdraft.com>
2017-08-23 23:02:53 +0000

    Direct association between foods and foods_details

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,10 @@
 class Food < ApplicationRecord
   # Direct associations
 
+  has_many   :foods_details,
+             :foreign_key => "foods_id",
+             :dependent => :destroy
+
   has_many   :cheat_foods,
              :foreign_key => "foods_id",
              :dependent => :destroy
