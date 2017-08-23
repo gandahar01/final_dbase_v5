f425b4cd1a23ff3d9d438df84628baa9d34f902f
Raghu Betina <raghu@firstdraft.com>
2017-08-23 23:03:28 +0000

    Whitelist columns in FoodsDetail for ActiveAdmin

--- app/admin/foods_detail.rb
+++ app/admin/foods_detail.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register FoodsDetail do
 
+ permit_params :foods_details_quantity, :foods_id, :nutrients_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
