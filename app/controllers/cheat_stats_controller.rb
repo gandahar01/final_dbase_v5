6d5d1d6a8c9d21c2d087d957c1aeb92cfd1c4959
Raghu Betina <raghu@firstdraft.com>
2017-08-23 23:03:28 +0000

    Display only signed in users cheat_stats on index

--- app/controllers/cheat_stats_controller.rb
+++ app/controllers/cheat_stats_controller.rb
@@ -10,7 +10,7 @@ class CheatStatsController < ApplicationController
   end
 
   def index
-    @cheat_stats = CheatStat.all
+    @cheat_stats = current_user.cheat_stats.page(params[:page]).per(10)
 
     render("cheat_stats/index.html.erb")
   end
