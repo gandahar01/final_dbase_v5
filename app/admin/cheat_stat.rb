dbca43e36f62717656ed304bf6a6c5ea1ef7d4eb
Raghu Betina <raghu@firstdraft.com>
2017-08-23 23:03:20 +0000

    Whitelist columns in CheatStat for ActiveAdmin

--- app/admin/cheat_stat.rb
+++ app/admin/cheat_stat.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CheatStat do
 
+ permit_params :calorie_bank, :cheat_days_total, :workout_counter, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
