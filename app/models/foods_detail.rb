da73036f11b66d347b323daed1add74fa0a499c4
Raghu Betina <raghu@firstdraft.com>
2017-08-23 23:02:53 +0000

    Direct association between nutrients and foods_details

--- app/models/foods_detail.rb
+++ app/models/foods_detail.rb
@@ -1,6 +1,9 @@
 class FoodsDetail < ApplicationRecord
   # Direct associations
 
+  belongs_to :nutrients,
+             :class_name => "Nutrient"
+
   belongs_to :foods,
              :class_name => "Food"
 
--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -1,6 +1,10 @@
 class Nutrient < ApplicationRecord
   # Direct associations
 
+  has_many   :foods_details,
+             :foreign_key => "nutrients_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
